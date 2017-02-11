bc4731ecef9b2bb2ae69bab86293ead3a0cc855a
Pierre-Jean Bergeron <pierrejeanbergeron@gmail.com>
2017-02-12 00:05:08 +0100

    Update webtorrent

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var client = new WebTorrent()
 
-if (window.location.hash) {    
+if (window.location.hash) {
   $('#content-goes-here').html('Downloading page from seeds...');
   $('#new-page').show()
-  client.download('magnet:?xt=urn:btih:' + document.URL.substr(document.URL.indexOf('#') + 1), function (torrent) {
+  client.add('magnet:?xt=urn:btih:' + document.URL.substr(document.URL.indexOf('#') + 1), function (torrent) {
     swarm = torrent.swarm
 
     $('#seeds-count').html(swarm.wires.length + 1)
@@ -23,20 +23,20 @@ if (window.location.hash) {
 else
 {
   $('#new-page').show()
-  
+
   $("form").submit(function(e) {
     e.preventDefault();
-    
+
     var f = new File([$('#new-page-content').val()], "html");
-    
+
     $('#content-goes-here').html($('#new-page-content').val())
 
-    client.seed(f, function onseed (torrent) {        
+    client.seed(f, function onseed (torrent) {
       magnetURI = torrent.magnetURI.split(':')[3].split('&')[0]
       document.location.hash = magnetURI;
-      
+
       swarm = torrent.swarm
-      
+
       swarm.on('wire', function(wire) {
         $('#seeds-count').html(swarm.wires.length + 1)
       });
