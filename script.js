afe91307d0d95d2772f8462abb0b8858278fc975
Pierre-Jean Bergeron <pierrejeanbergeron@gmail.com>
2017-02-12 00:17:43 +0100

    Adding trackers

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-var client = new WebTorrent()
+$(document).ready(function() {
+  var client = new WebTorrent()
 
-if (window.location.hash) {
-  $('#content-goes-here').html('Downloading page from seeds...');
-  $('#new-page').show()
-  client.add('magnet:?xt=urn:btih:' + document.URL.substr(document.URL.indexOf('#') + 1), function (torrent) {
-    swarm = torrent.swarm
-
-    $('#seeds-count').html(swarm.wires.length + 1)
-
-    swarm.on('wire', function(wire) {
-      $('#seeds-count').html(swarm.wires.length + 1)
-    });
-
-    var file = torrent.files[0]
-
-    file.getBuffer(function (err, buffer) {
-      if (err) throw err
-        $('#content-goes-here').html(buffer.toString('utf8'))
-    })
-  });
-}
-else
-{
-  $('#new-page').show()
-
-  $("form").submit(function(e) {
-    e.preventDefault();
-
-    var f = new File([$('#new-page-content').val()], "html");
-
-    $('#content-goes-here').html($('#new-page-content').val())
-
-    client.seed(f, function onseed (torrent) {
-      magnetURI = torrent.magnetURI.split(':')[3].split('&')[0]
-      document.location.hash = magnetURI;
-
+  if (window.location.hash) {
+    $('#content-goes-here').html('Downloading page from seeds...');
+    $('#new-page').show()
+    client.add('magnet:?xt=urn:btih:' + document.URL.substr(document.URL.indexOf('#') + 1) + '&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com', function (torrent) {
       swarm = torrent.swarm
+
+      $('#seeds-count').html(swarm.wires.length + 1)
 
       swarm.on('wire', function(wire) {
         $('#seeds-count').html(swarm.wires.length + 1)
       });
+
+      var file = torrent.files[0]
+
+      file.getBuffer(function (err, buffer) {
+        if (err) throw err
+          $('#content-goes-here').html(buffer.toString('utf8'))
+      })
     });
-    return false;
-  });
-}
+  }
+  else
+  {
+    $('#new-page').show()
+
+    $("form").submit(function(e) {
+      e.preventDefault();
+
+      var f = new File([$('#new-page-content').val()], "html");
+
+      $('#content-goes-here').html($('#new-page-content').val())
+
+      client.seed(f, function onseed (torrent) {
+        magnetURI = torrent.magnetURI.split(':')[3].split('&')[0]
+        document.location.hash = magnetURI;
+
+        swarm = torrent.swarm
+
+        swarm.on('wire', function(wire) {
+          $('#seeds-count').html(swarm.wires.length + 1)
+        });
+      });
+      return false;
+    });
+  }
+})
